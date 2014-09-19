8985b62bb9e5b49a93d12f14bf8d4c1f5db794d0
FedeParkinson <bro_fede@hotmail.com>
2014-09-18 17:24:17 -0700

    FGp

---
+++ FGP.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+cout << "FFFFFFFF   GGGGGGGG    PPPPPPPPP" << endl;
+cout << "F         GG           P       P" << endl;
+cout << "F         GG           P       P" << endl;
+cout << "FFFFFFF   GG           PPPPPPPPP" << endl;
+cout << "F         GG    GGGGG  P" << endl;
+cout << "F         GG        G  P" << endl;
+cout << "F          GG      GG  P" << endl;
+cout << "F           GGGGGGG    P" << endl;
+return 0;
+} 
